e859fc74739203766bc0e7833cfacffe19810922
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-22 14:23:19 -0300

    eliminado metodo main innecesario

--- src/main/java/com/jmr/practica/entities_practica/libreria_custom_users_practica/LibreriaCustomUsersPracticaApplication.java
+++ src/main/java/com/jmr/practica/entities_practica/libreria_custom_users_practica/LibreriaCustomUsersPracticaApplication.java
@@ -1,14 +1,8 @@
 package com.jmr.practica.entities_practica.libreria_custom_users_practica;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class LibreriaCustomUsersPracticaApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(LibreriaCustomUsersPracticaApplication.class, args);
-    }
-
 }
