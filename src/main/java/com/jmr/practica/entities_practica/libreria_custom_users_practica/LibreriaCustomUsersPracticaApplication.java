fb93cd6a2a531a57d9e5847da606c9ab787601c9
jmRicardo <51313148+jmRicardo@users.noreply.github.com>
2021-02-22 12:20:00 -0300

    iniciando proyecto para guardar entities del tipo usuario

---
+++ src/main/java/com/jmr/practica/entities_practica/libreria_custom_users_practica/LibreriaCustomUsersPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.entities_practica.libreria_custom_users_practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaCustomUsersPracticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibreriaCustomUsersPracticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmr/practica/entities_practica/libreria_custom_users_practica/LibreriaCustomUsersPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmr.practica.entities_practica.libreria_custom_users_practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaCustomUsersPracticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
